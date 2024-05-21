b742d6ea80f21f494c8b435b65dba50f9ff8f009
Javier Garcia Mourelo <javimourelo@hotmail.com>
2024-05-21 23:30:10 +0000

    Finalizado

--- src/js/app.js
+++ src/js/app.js
@@ -5,10 +5,10 @@ import "../style/index.css";
  *  This function is called every time the user changes types or changes any input
  * 
     {
-        includeCover: true, // if includeCover is true the algorithm should show the cover image
-        background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the image's url that will be used as a background for the profile cover
+        includeCover: true, // if includeCover is true the algorithm should
+        background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the url of the image that will used as background for the profile cover
         avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
-        socialMediaPosition: "right", // social media bar position (left or right)
+        socialMediaPosition: "left", // social media bar position (left or right)
         
         twitter: null, // social media usernames
         github: null,
@@ -16,31 +16,70 @@ import "../style/index.css";
         instagram: null,
 
         name: null,
-        lastName: null,
+        lastname: null,
         role: null,
         country: null,
         city: null
     }
  */
 function render(variables = {}) {
-  console.log("These are the current variables: ", variables); // print on the console
+  console.log("These are the current variables: ", variables); //print on the console
   // here we ask the logical questions to make decisions on how to build the html
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
-
   // reset the website body with the new html output
+  let initialName = "";
+  variables.name == null
+    ? (initialName = "Jane")
+    : (initialName = variables.name);
+  let lastName = "";
+  variables.lastname == null
+    ? (lastName = "Doe")
+    : (lastName = variables.lastname);
+  // let sMposition = "";
+  // variables.socialMediaPosition == "position-left"
+  //   ? (sMposition = "position-left")
+  //   : (sMposition = "position-right");
+  let personRole = "";
+  variables.role == null
+    ? (personRole = "Human")
+    : (personRole = variables.role);
+  let personCity = "";
+  variables.city == null
+    ? (personCity = "City")
+    : (personCity = variables.city);
+  let personCountry = "";
+  variables.country == null
+    ? (personCountry = "Country")
+    : (personCountry = variables.country);
+  let twitterHandle = "";
+  variables.twitter == null
+    ? (twitterHandle = "4geeksacademy")
+    : (twitterHandle = variables.twitter);
+  let githubHandle = "";
+  variables.github == null
+    ? (githubHandle = "4geeksacademy")
+    : (githubHandle = variables.github);
+  let linkedinHandle = "";
+  variables.linkedin == null
+    ? (linkedinHandle = "4geeksacademy")
+    : (linkedinHandle = variables.linkedin);
+  let instagramHandle = "";
+  variables.instagram == null
+    ? (instagramHandle = "4geeksacademy")
+    : (instagramHandle = variables.instagram);
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${initialName} ${lastName}</h1>
+          <h2>${personRole}</h2>
+          <h3>${personCity}, ${personCountry}</h3>
+          <ul class=${variables.socialMediaPosition}>
+            <li><a href="https://twitter.com/${twitterHandle}"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${githubHandle}"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${linkedinHandle}"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${instagramHandle}"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -51,9 +90,9 @@ function render(variables = {}) {
  */
 window.onload = function() {
   window.variables = {
-    // if includeCover is true the algorithm should show the cover image
+    // if includeCover is true the algorithm should
     includeCover: true,
-    // this is the image's url that will be used as a background for the profile cover
+    // this is the url of the image that will used as background for the profile cover
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
     // this is the url for the profile avatar
     avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
@@ -65,12 +104,12 @@ window.onload = function() {
     linkedin: null,
     instagram: null,
     name: null,
-    lastName: null,
+    lastname: null,
     role: null,
     country: null,
     city: null
   };
-  render(window.variables); // render the card for the first time
+  render(window.variables); //render the card for the first time
 
   document.querySelectorAll(".picker").forEach(function(elm) {
     elm.addEventListener("change", function(e) {
@@ -85,7 +124,7 @@ window.onload = function() {
           : this.value == "false"
           ? false
           : this.value;
-      render(Object.assign(window.variables, values)); // render again the card with new values
+      render(Object.assign(window.variables, values)); // render again the card with new valus
     });
   });
 };
